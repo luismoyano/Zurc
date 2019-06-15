76d3f09b2306a83761c2f4e479c526d25575cff4
Luis Moyano <luis@luismoyano.net>
2019-06-15 19:44:34 +0200

    Input manager function fed up

--- Zurc/World.cpp
+++ Zurc/World.cpp
@@ -1,9 +1,11 @@
 #include "pch.h"
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <vector>
 #include <conio.h>
 #include "World.h"
+#include "Instructions.h"
 
 using namespace std;
 
@@ -19,12 +21,33 @@ World::~World()
 
 bool World::parseInput(string& input)
 {
+	//Lowercase input
+	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+	
 	//Assume good input, only return false if the input is not within our knowledge
 	bool returnable = true;
 
-	if(input == "look")
+	if(input == INSTRUCTION_LOOK)
 	{
 		cout << "The Zucc loocc";
+	}
+	else if (input == INSTRUCCTION_UP)
+	{
+
+	}
+	else if(input == INSTRUCCTION_DOWN)
+	{
+	}
+	else if(input == INSTRUCCTION_LEFT)
+	{
+	}
+	else if (input == INSTRUCCTION_RIGHT)
+	{
+
+	}
+	else
+	{
+		returnable = false;
 	}
 
 	return returnable;
