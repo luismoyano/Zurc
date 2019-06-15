e8bbb2db615e8ff40af8cbf7805842879fdf29b8
Luis Moyano <luis@luismoyano.net>
2019-06-15 19:50:31 +0200

    Basic player class created

---
+++ Zurc/Player.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Player.h"
+#include <string>
+
+
+
+Player::Player()
+{
+}
+
+
+Player::~Player()
+{
+}
+
+void Player::look()
+{
+}
+
+void Player::move(string where)
+{
+}
---
+++ Zurc/Player.h
@@ -0,0 +1,15 @@
+#pragma once
+
+using namespace std;
+
+class Player
+{
+public:
+	Player();
+	~Player();
+
+	void look();
+
+	void move(string where);
+};
+
